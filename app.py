78f347114c6a42c42b1e13ab8c4cca40272a08b3
Britten Hanson <britten.hanson11@gmail.com>
2024-07-10 08:02:52 -0600

    add app.py, models starting with User, base template with bootswatch, gitignore for venv

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+
+app = Flask(__name__)
+app.app_context().push()
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///authentication"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "secretauth"
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
