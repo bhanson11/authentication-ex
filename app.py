e17f89fdb080dafad35ef0d7a07061bf1de59ae1
Britten Hanson <britten.hanson11@gmail.com>
2024-07-11 15:51:41 -0600

    add into base.html and app.py to connect to debug toolbar and database

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from models import connect_db, db, User
 
 app = Flask(__name__)
 app.app_context().push()
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///authentication"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///authentication"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "secretauth"
 # app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 connect_db(app)
-db.create_all()
+# db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
