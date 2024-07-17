eb4f7dfb6794d43524d1f8d2c217bcf8f89a53f4
Britten Hanson <britten.hanson11@gmail.com>
2024-07-16 19:09:31 -0600

    add RegisterForm and LoginForm in forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,16 @@
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, Email
 
 class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(max=20)])
+    password = PasswordField('Password', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    submit = SubmitField('Regsiter')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(max=20)])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
