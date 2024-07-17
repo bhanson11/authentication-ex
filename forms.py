5c38323f069bf91e61fd345f82c91729d179de9c
Britten Hanson <britten.hanson11@gmail.com>
2024-07-16 19:11:24 -0600

    add limitations on character numbers for username, email, first_name and last_name

--- forms.py
+++ forms.py
@@ -6,8 +6,8 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(max=20)])
     password = PasswordField('Password', validators=[InputRequired()])
     email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
-    first_name = StringField('First Name', validators=[InputRequired()])
-    last_name = StringField('Last Name', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+    last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
     submit = SubmitField('Regsiter')
 
 class LoginForm(FlaskForm):
