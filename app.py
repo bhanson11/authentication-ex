378adafb899c048ffcd5cf27120bdd31966d6476
Britten Hanson <britten.hanson11@gmail.com>
2024-07-24 11:03:11 -0600

    create logout button, finish delete user app.route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, session
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, redirect, session, url_for
 from models import connect_db, db, User, bcrypt
 from forms import RegisterForm, LoginForm
+from functools import wraps
 
 app = Flask(__name__)
 app.app_context().push()
@@ -14,7 +14,15 @@ app.config["SECRET_KEY"] = "secretauth"
 connect_db(app)
 db.create_all()
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "username" not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/', methods=['GET'])
 def home():
@@ -61,6 +69,7 @@ def login():
             return redirect(f"/users/{username}")
         else: 
             form.username.errors.append('Invalid username/password')
+            return render_template("login.html", form=form)
     
     return render_template("login.html", form=form)
 
@@ -71,12 +80,43 @@ def logout():
     return redirect("/login")
 
 @app.route('/users/<username>')
+@login_required
 def show_user(username):
     """user page for logged-in-users"""
 
     user = User.query.get(username)
 
-    if 'username' not in session:
+    if 'username' not in session or username != session['username']:
         return redirect('/login')
     
     return render_template("users/show.html", user=user)
+
+@app.route('/users/<username>/delete', methods=['POST'])
+@login_required
+def remove_user(username):
+    
+    user = User.query.get(username)
+
+    if 'username' in session != session['username']:
+        raise Exception("Unauthorized")
+    
+    db.User.delete(user)
+    session.pop("username")
+    db.session.commit()
+    
+
+
+# @app.route('/users/<username>/feedback/add', methods=['GET', 'POST'])
+# @login_required
+# def add_feedback():
+#     """GET to display form to add feedback and POST to add a new piece of feedback and redirect to users/<username> -- only allow user who is logged in to add feedback"""
+    
+# @app.route('/feedback/<feedback-id>/update', methods=['GET', 'POST'])
+# @login_required
+# def update_feedback():
+#     """Make sure that only the user who has written that feedback can update it."""
+
+# @app.route('/feedback/<feedback-id>/delete', methods=['POST'])
+# @login_required
+# def delete_feedback():
+#     """Make sure that only the user who has written that feedback can delete it."""
